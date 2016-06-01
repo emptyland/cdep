5fececf7d483a8acf491f76f1c9d5530d88259ee
emptyland <cjvoid@gmail.com>
2016-06-01 10:13:31 +0800

    Add travis CI config file.

---
+++ README.md
@@ -0,0 +1,4 @@
+cdep - A C/C++ package management tool
+========
+
+![](https://cdn.rawgit.com/LunaGao/BlessYourCodeTag/master/tags/god.svg)
--- libs/fetch_package.sh
+++ libs/fetch_package.sh
@@ -15,7 +15,6 @@ mkdir -p third-party/include
 mkdir -p third-party/libs
 mkdir -p third-party/${1}
 
-#rm -r third-party/${1}/*
 wget ${2} -O "third-party/${1}/${INCOMING_FILE_NAME}"
 
 cd third-party/${1}
@@ -24,11 +23,16 @@ typeMsg=$(file ${INCOMING_FILE_NAME})
 
 echo ${typeMsg} | grep 'gzip'
 if [[ $? == '0' ]]; then
-    tar vxzf "${INCOMING_FILE_NAME}" && echo ${2} > .download.snapshot
+    tar vxzf "${INCOMING_FILE_NAME}" || exit 1
+    echo ${2} > .download.snapshot
 fi
+
 echo ${typeMsg} | grep 'Zip'
 if [[ $? == '0' ]]; then
-    unzip "${INCOMING_FILE_NAME}" && echo ${2} > .download.snapshot
+    unzip "${INCOMING_FILE_NAME}" || exit 1
+    echo ${2} > .download.snapshot
 fi
+
+rm ${INCOMING_FILE_NAME}
 
 
